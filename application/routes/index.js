5d53477fe660b3d666054b7a30daf51c318e38e7
sderazo <sdiazera@gmail.com>
2022-11-05 14:12:17 -0700

    added .hbs and router statements

--- application/routes/index.js
+++ application/routes/index.js
@@ -6,4 +6,21 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'CSC 317 App', name:"Sabrina Diaz-Erazo" });
 });
 
+router.get("/login", function(req, res) {
+  res.render('login');
+})
+
+router.get("/register", function(req, res) {
+  res.render('registration');
+})
+
+router.get("/postimage", function(req, res) {
+  res.render('postimage');
+})
+
+// what to write for viewpost for now
+router.get("/posts/:id", function(req, res) {
+  res.render('viewpost');
+})
+
 module.exports = router;
