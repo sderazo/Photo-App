044d85a727d18e29d383645918e2a551d4c18ac0
sderazo <sdiazera@gmail.com>
2022-11-21 10:25:41 -0800

    added name to registration.hbs

--- application/routes/users.js
+++ application/routes/users.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+//Method: POST
+//localhost:3000/users/register
 router.post("/register", function(req, res) {
-
-
+  console.log(req.body);
+  res.send();
 });
 
+//Method: POST
+//localhost:3000/users/login
 router.post("/login", function(req, res) {
 
 
