d178f6e394f2535ebfb26bdbc036cfef24d8f033
sderazo <sdiazera@gmail.com>
2022-12-04 14:02:27 -0800

    changed index.hbs to match prof and uploaded images

--- application/routes/posts.js
+++ application/routes/posts.js
@@ -19,8 +19,31 @@ const storage = multer.diskStorage({
   const upload = multer({ storage: storage })
 
 router.post("/create", upload.single("uploadImage") , function(req,res,next){
-    console.log(req);
-    res.send();
+    let uploadedFile = req.file.path;
+    let thumbnailName = `thumbnail-${req.file.filename}`;
+    let destinationOfThumbnail = `${req.file.destination}/${thumbnailName}`;
+    const {title, description} = req.body;
+    const userId = req.session.userId;
+
+    sharp(uploadedFile)
+        .resize(200)
+        .toFile(destinationOfThumbnail)
+        .then(function() {
+            let baseSQL = `
+            INSERT INTO posts (title, description, image, thumbnail, fk_authorId) VALUE (?,?,?,?,?)
+            `
+            return db.query(baseSQL, [title,description,uploadedFile, destinationOfThumbnail,userId])
+        })
+        .then(function([results,fields]){
+            if(results && results.affectedRows){
+                req.flash("success", "Your post has been created!");
+                req.session.save(function(saveErr){
+                    res.redirect('/');
+                })
+            }
+        })
+        .catch(err => next(err));
+
 })
 
 module.exports = router;
