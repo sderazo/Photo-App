dea183dd7d325233451018483fed1dafb3eff424
sderazo <sdiazera@gmail.com>
2022-12-04 05:37:46 -0800

    edit and add multer to posts.js

--- application/routes/posts.js
+++ application/routes/posts.js
@@ -1,21 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const sharp = require('sharp');
 
 const db = require('../conf/database');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, '/tmp/my-uploads')
+      cb(null, 'public/images/uploads')
     },
     filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, file.fieldname + '-' + uniqueSuffix)
+      // image/jpeg
+      let fileExt = file.mimetype.split("/")[1];
+      cb(null, `${file.fieldname}-${Date.now()}-${Math.round(Math.random() * 1E9)}.${fileExt}`)
     }
   })
   
   const upload = multer({ storage: storage })
 
-router.post("/create", function(req,res,next){
+router.post("/create", upload.single("uploadImage") , function(req,res,next){
     console.log(req);
     res.send();
 })
