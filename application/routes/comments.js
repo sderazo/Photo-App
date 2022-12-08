7760c1db714ed4c4fa4001a9ebe44a2c00b13d04
sderazo <sdiazera@gmail.com>
2022-12-08 11:14:54 -0800

    added db.execute to comments.js

--- application/routes/comments.js
+++ application/routes/comments.js
@@ -3,8 +3,16 @@ const router = express.Router();
 const db = require('../conf/database');
 
 router.post("/create", function(req,res,next){
-    console.log(req.body);
-    res.json(req.body);
+    if(!req.session.userId){
+        res.json({
+            status:"error",
+            message:"You must be logged in"
+        });
+    }else{
+        let {comment, postId} = req.body;
+        let {userId, username} = req.session;
+        db.execute(`INSERT INTO comments (text, fk_authorId, fk_postId) value (?,?,?);`)
+    }
 })
 
 module.exports = router
