6be1160f34068c341fc37ed96de9dc77a6299582
sderazo <sdiazera@gmail.com>
2022-12-09 20:08:50 -0800

    added more pics, comments and show 12 images in index

--- application/middleware/posts.js
+++ application/middleware/posts.js
@@ -1,7 +1,7 @@
 const db = require('../conf/database');
 module.exports = {
     getRecentPosts: function(req,res,next){
-        db.query('select id, title, description, thumbnail from posts ORDER BY createdAt DESC LIMIT 8')
+        db.query('select id, title, description, thumbnail from posts ORDER BY createdAt DESC LIMIT 12')
         .then(function([results, fields]){
             if(results && results.length){
                 res.locals.results = results;
